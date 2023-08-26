b24273ac8f458196482a0810f67e81d73c770c7c
NiranjanxPatil <niranjan41210@gmail.com>
2023-08-27 00:17:54 +0530

    changes

--- app/sections/project/ProjectItem.jsx
+++ app/sections/project/ProjectItem.jsx
@@ -1,9 +1,4 @@
-
-// ProjectItem.jsx
-// Mark ProjectItem as a client component
-// use client
-"use client"
-import { useRef } from "react";
+import React, { useRef } from "react";
 import { useInView } from "framer-motion";
 import Link from "next/link";
 import ImageGallery from "react-image-gallery";
@@ -34,74 +29,72 @@ export function ProjectItem({ project, index }) {
         transition: `all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) ${
           index === 0 ? 0 : 25 * index
         }ms`,
-    }}
+      }}
     >
-        <figure>
-            <div className="aspect-[12/9.2] w-full h-full">
-                <Suspense fallback={<Loader />}>
-                    <ImageGallery
-                        items={galleryImages}
-                        showPlayButton={false}
-                        showThumbnails={false}
-                        showIndex
-                        lazyload
-                        additionalClass="gallery-item"
-                    />
-                </Suspense>
-            </div>
-        </figure>
-
-        <div className="flex-[2] px-5 py-6 text-center flex flex-col gap-10">
-            <header className="flex-1 flex items-center justify-start flex-col gap-3">
-                <h3 tabIndex="0" className="text-2xl font-bold">
-                    {title}
-                </h3>
-                <p tabIndex="0" className="leading-7 font-light">
-                    {description}
-                </p>
-            </header>
-
-            <footer className="flex flex-col gap-10">
-                {!!stack.length && (
-                    <div className="flex-center flex-wrap gap-3">
-                        {stack.map((tag) => (
-                            <span
-                                key={tag}
-                                tabIndex="0"
-                                className="px-2 text-sm leading-normal rounded bg-badge-light/50 dark:bg-badge-dark"
-                            >
-                                {tag}
-                            </span>
-                        ))}
-                    </div>
-                )}
-
-                <div className="flex-center gap-10">
-                    {repoUrl && (
-                        <Link
-                            href={repoUrl}
-                            target="_blank"
-                            className="icon-link-btn"
-                            title="Go to Github repository"
-                        >
-                            <VscSourceControl />
-                            <span>Source</span>
-                        </Link>
-                    )}
-                    {liveUrl && (
-                        <Link
-                            href={liveUrl}
-                            target="_blank"
-                            className="icon-link-btn"
-                            title="Go to live address"
-                        >
-                            <FiExternalLink />
-                            <span>Demo</span>
-                        </Link>
-                    )}
-                </div>
-            </footer>
+      <figure>
+        <div className="aspect-[12/9.2] w-full h-full">
+          <ImageGallery
+            items={galleryImages}
+            showPlayButton={false}
+            showThumbnails={false}
+            showIndex
+            lazyload
+            additionalClass="gallery-item"
+          />
         </div>
+      </figure>
+
+      <div className="flex-[2] px-5 py-6 text-center flex flex-col gap-10">
+        <header className="flex-1 flex items-center justify-start flex-col gap-3">
+          <h3 tabIndex="0" className="text-2xl font-bold">
+            {title}
+          </h3>
+          <p tabIndex="0" className="leading-7 font-light">
+            {description}
+          </p>
+        </header>
+
+        <footer className="flex flex-col gap-10">
+          {!!stack.length && (
+            <div className="flex-center flex-wrap gap-3">
+              {stack.map((tag) => (
+                <span
+                  key={tag}
+                  tabIndex="0"
+                  className="px-2 text-sm leading-normal rounded bg-badge-light/50 dark:bg-badge-dark"
+                >
+                  {tag}
+                </span>
+              ))}
+            </div>
+          )}
+
+          <div className="flex-center gap-10">
+            {repoUrl && (
+              <Link
+                href={repoUrl}
+                target="_blank"
+                className="icon-link-btn"
+                title="Go to Github repository"
+              >
+                <VscSourceControl />
+                <span>Source</span>
+              </Link>
+            )}
+            {liveUrl && (
+              <Link
+                href={liveUrl}
+                target="_blank"
+                className="icon-link-btn"
+                title="Go to live address"
+              >
+                <FiExternalLink />
+                <span>Demo</span>
+              </Link>
+            )}
+          </div>
+        </footer>
+      </div>
     </article>
   );
 }
